7da0ef14e4976d82ad12f4160215eb738cf4058b
AntoineLambert <lambert.antoine@hotmail.fr>
2016-06-27 15:10:21 +0200

    File to use to create the same DataBase for all. Use it Once only !

---
+++ dbcreate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import rethinkdb as r
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from threading import Thread
+#initialise le serveur sockio avec Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+#crée la Table sur RethinkDB
+r.connect( "localhost", 28015).repl()
+r.db_create('chatDB').run()
+r.db("chatDB").table_create("name").run()
+r.db("chatDB").table_create("message").run()
+r.db("chatDB").table_create("time").run()
+r.db("chatDB").table_create("password").run()
+r.db("chatDB").table_create("email").run()
+
+
+print('🐱')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
