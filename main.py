98c20c52c72a13984a0faab019dfadfd7fd04f9f
AntoineLambert <lambert.antoine@hotmail.fr>
2016-06-27 15:23:36 +0200

    update comment

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from threading import Thread
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-#crée la Table sur RethinkDB
+#se connecter à RethinkDB
 r.connect( "localhost", 28015).repl()
 
 
 
-print('🐱')
 
+print('🐱')
 
 if __name__ == '__main__':
     socketio.run(app)
