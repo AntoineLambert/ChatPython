9c752a196331a4cfd0dd9856c3c6e6533d6b92ff
AntoineLambert <lambert.antoine@hotmail.fr>
2016-06-28 17:17:48 +0200

    Commit Ready to send Messages To DB

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import rethinkdb as r
+import rethinkdb as rdb
 import flask
-from flask import Flask, render_template, request
+from flask import Flask, request, redirect, flash, render_template, url_for
+from flask import g, jsonify, render_template, request, abort, make_response, session
 from flask_socketio import SocketIO
 from threading import Thread
+import json
 #initialise le serveur sockio avec Flask
-app = flask.Flask(__name__)
+app = Flask(__name__, static_url_path='')
+app.config.from_object(__name__)
 
-#se connecter à RethinkDB
-conn = r.connect( "localhost", 28015).repl()
+app.config.update(dict(DEBUG=True,
+                       RDB_HOST='localhost',
+                       RDB_PORT=28015,
+                       DB_NAME='chatDB',
+                       TABLE_NAME='messages'))
+
+# open connection before each request
+@app.before_request
+def before_request():
+    try:
+        g.conn = rdb.connect(host=app.config['RDB_HOST'], port=app.config['RDB_PORT'], db=app.config['DB_NAME'])
+    except RqlDriverError:
+        abort(503, "Database connection could be established.")
+
+@app.teardown_request
+def teardown_request(exeption):
+	try:
+		g.conn.close()
+	except AttributeError:
+		pass
 
 @app.route('/')
 def my_form():
@@ -17,12 +38,11 @@ def my_form():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-
 	text = request.form['text']
 	print text
-	r.db("chatDB").table("message").insert(text).run(conn)
+	rdb.db(app.config['DB_NAME']).table(app.config['TABLE_NAME']).insert({'text': text}).run(g.conn)
 	return my_form()
-	
+
 print('🐱')
 
 if __name__ == '__main__':
