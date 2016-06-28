4254dbbd93f530dc3afc442debb02b3c9436060b
AntoineLambert <lambert.antoine@hotmail.fr>
2016-06-28 11:01:26 +0200

    Commit Miaou

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import rethinkdb as r
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from threading import Thread
 #initialise le serveur sockio avec Flask
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+app = flask.Flask(__name__)
 
 #se connecter à RethinkDB
-r.connect( "localhost", 28015).repl()
+conn = r.connect( "localhost", 28015).repl()
 
+@app.route('/')
+def my_form():
+	return flask.render_template("index.html")
 
+@app.route('/', methods=['POST'])
+def my_form_post():
 
-
+	text = request.form['text']
+	print text
+	r.db("chatDB").table("message").insert(text).run(conn)
+	return my_form()
+	
 print('🐱')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
